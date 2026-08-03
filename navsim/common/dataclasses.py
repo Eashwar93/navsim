ee0f462c8bd70054b360a17fc637d57fec8bd85e
Andreas Bär <andreas.baer@vinfastauto.com>
2026-08-03 15:25:33 +0200

    transform driving command to one-hot numpy array

--- navsim/common/dataclasses.py
+++ navsim/common/dataclasses.py
@@ -272,7 +272,8 @@ class AgentInput:
         ego_pose = ego_state_se3.center_se3.array
         ego_velocity = dynamic_state_se3.velocity_2d.array
         ego_acceleration = dynamic_state_se3.acceleration_2d.array
-        driving_command = agent_api.get_driving_command_heuristic()
+        driving_command = np.zeros(4, dtype=np.float32)
+        driving_command[agent_api.get_driving_command_heuristic()] = 1
 
         return EgoStatus(
             ego_pose=ego_pose,
